00d469ba3f9644ed6de66c3826dff4f77a49c707
RayyanNafees <nafees.rayyan@gmail.com>
2023-07-25 19:13:51 +0530

    COnverted data to different sources

--- README.md
+++ README.md
@@ -11,3 +11,14 @@ Pixel Cipher implementation on the web via WebAssembly for data conversion
 - [Converting a string of binary number to Integer](https://www.tutorialspoint.com/convert-an-array-of-binary-numbers-to-corresponding-integer-in-javascript)
 - [Converting Integer to binary string](https://stackoverflow.com/questions/11103487/how-to-convert-binary-representation-of-number-from-string-to-integer-number-in)
 - [Base64 conversion](https://stackoverflow.com/questions/6213227/fastest-way-to-convert-a-number-to-radix-64-in-javascript)
+- [Image from TypeArray of RGBA Pixels](https://stackoverflow.com/questions/22823752/creating-image-from-array-in-javascript-and-html5)
+- [ArrayBuffer & Base64 interconversion](https://stackabuse.com/encoding-and-decoding-base64-strings-in-node-js/)
+
+## Base64 & ArrayBuffers
+```js
+const encoded = Buffer.from('username:password', 'utf8').toString('base64')  
+// 'dXNlcm5hbWU6cGFzc3dvcmQ='
+
+const plain = Buffer.from('dXNlcm5hbWU6cGFzc3dvcmQ=', 'base64').toString('utf8')  
+// 'username:password'
+```
--- src/utils/fileread.ts
+++ src/utils/fileread.ts
@@ -1,10 +1,35 @@
-export type Data = string | number | ArrayBuffer | null
+import { Base64 } from './filedata'
+
+export type Data =
+  | string
+  | number
+  | ArrayBuffer
+  | null
+  | Uint8Array
+  | DataView
+  | any
+
+export const Q = <T extends HTMLElement>(q: string) =>
+  document.querySelector<T>(q)!
 
 export const toDataURL = (file: File): Promise<Data> =>
   new Promise((resolve, reject) => {
     const reader = new FileReader()
     reader.readAsDataURL(file)
     reader.onload = () => resolve(reader.result)
+    reader.onerror = reject
+  })
+
+export const toImg = (imgId: string, file: File): Promise<Data> =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () =>
+      resolve(
+        (Q<HTMLImageElement>(
+          `img`
+        ).src = `${reader.result}`)
+      )
     reader.onerror = reject
   })
 
@@ -34,9 +59,20 @@ export const toText = (file: File): Promise<Data> =>
 
 /**
  * Returns a promise that writes the data returned by the `promise` into `elem` Element's innerText
- * @param {HTMLElement} elem The element to input the data as returned by `promise`
+ * @param {string} elem The element ID to input the data as returned by `promise`
  * @param {Promise<Data>} promise The promise that resolves to returning the data
  */
-export const output = async (elem: HTMLElement, promise: Promise<Data>) => {
-  await promise.then((data: Data) => (elem.innerHTML = String(data)))
-}
+export const output = async (
+  elemId: string,
+  promise: Promise<Data>
+): Promise<string> =>
+  await promise.then((data: Data) => {
+    const str_data = String(data)
+
+    // Raw data as text into div>div
+    Q(`#${elemId} div`).innerText = str_data
+
+    // Text Length into div>h4>span
+    Q(`#${elemId} span`).innerText = String(str_data.replace(',', '').length)
+    return str_data
+  })
